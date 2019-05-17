b1319afebffdbef415366f1d79092375398ca069
wangwei <wangwei_ios@smartisan.com>
2019-05-17 15:04:59 +0800

    project init

---
+++ GaoDeMapTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GaoDeMapTest
+//
+//  Created by waw on 2019/5/17.
+//  Copyright © 2019 FB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
